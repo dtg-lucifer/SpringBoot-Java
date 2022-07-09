2f15a5ddfa307e613323098286c7fd402e3173ee
dtg-lucifer <piushbasu@gmail.com>
2022-07-09 22:30:14 +0530

    Initial

--- src/main/java/com/firstapplication/main/MainApplication.java
+++ src/main/java/com/firstapplication/main/MainApplication.java
@@ -1,10 +1,13 @@
 package com.firstapplication.main;
 
+import com.firstapplication.main.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,15 @@ public class MainApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World", "This is my first REST application in JAVA");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Student 1",
+						"student1@gmail.com",
+						LocalDate.of(2005, Month.JANUARY, 19),
+						21
+				)
+		);
 	}
 }
